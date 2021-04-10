a868f9a6c64d3574327fdbf13d80d5456bcb7d40
shirazimam <shiraz.imam@gmail.com>
2021-04-10 20:08:23 +0530

    opk

--- src/controllers/submission.controller.js
+++ src/controllers/submission.controller.js
@@ -22,6 +22,7 @@ const document = catchAsync(async (req, res) => {
 
 const profile = catchAsync(async (req, res) => {
   const data = await submissionService.updateprofile(req.body.user, req.file);
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(httpStatus.OK).send({ data });
 });
 

8ea7922301805fd122d308ca8324f7a605800158
shirazimam <shiraz.imam@gmail.com>
2021-04-10 20:14:08 +0530

    opk

--- src/app.js
+++ src/app.js
@@ -53,7 +53,19 @@ app.use(cors({
     return callback(null, true);
   }
 }));
-app.options('*', cors());
+app.options('*', cors({
+  origin: function(origin, callback){
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // jwt authentication
 app.use(passport.initialize());
